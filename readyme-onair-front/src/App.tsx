e9f3c848884eb8581b7a16206ceabf24ab3187dd
ilmerry <milkiss0414@naver.com>
2022-03-08 11:41:38 +0900

    ADD_styles-default

--- readyme-onair-front/src/App.tsx
+++ readyme-onair-front/src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Header from "./Components/Header";
-import Router from "./Router";
+import React from 'react';
+import Header from './Components/Header';
+import Router from './Router';
 
 const App = () => {
   return (
---
+++ readyme-onair-front/src/theme.ts
@@ -0,0 +1,6 @@
+import {DefaultTheme} from "styled-components"
+
+export const theme:DefaultTheme = {
+    bgColor:"#FFFFF",
+    accentColor:"#2152F4"
+}
