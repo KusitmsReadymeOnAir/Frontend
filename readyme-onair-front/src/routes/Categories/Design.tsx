495e0a384c7b70c9ff1317b0f541d6ee79b95c7d
Sae-byeol <toquf0424@naver.com>
2022-03-10 22:49:54 +0900

    ADD: 카테고리 기능

--- readyme-onair-front/src/routes/Categories/Design.tsx
+++ readyme-onair-front/src/routes/Categories/Design.tsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 // 게시글 리스트 - 카테고리: 디자인
 
 const Design = () => {
-  return <div>Design</div>;
+  //해당 카테고리에 맞게 api 요청하여 게시글 띄움
+  const [posts, setPosts]=useState([
+    {
+      _id: "6226fed1aa058eaab230059e",
+      title: "디자인1",
+      content: "테스트 내용",
+      category: 1,
+      writer: "test1",
+      pw: 1234,
+      imageId: "이미지 번호",
+      date: "2022-03-08T06:59:29.920Z"
+  },
+  {
+    _id: "6226fed1aa058eaab230059e",
+    title: "디자인2",
+    content: "테스트 내용",
+    category: 1,
+    writer: "test1",
+    pw: 1234,
+    imageId: "이미지 번호",
+    date: "2022-03-08T06:59:29.920Z"
+},
+  ])
+  return <div>
+    <PostsContainer>
+    {posts.map((item)=>{
+          return (
+            <Link to={`/post/${item._id}`} style={{textDecoration:"none"}}>
+              <Card>
+                <img src="../imgs/Image.png"></img>
+                <div>{item.title}</div>
+              </Card>  
+            </Link>
+        )})}
+    </PostsContainer>
+   
+  </div>;
 };
-
+const PostsContainer=styled.div`
+  display:grid ;
+  grid-template-columns:repeat(auto-fill, minmax(33%,33%));
+  margin-top:50px ;
+  row-gap:30px;
+  grid-template-rows:1fr 1fr 1fr;
+  text-align:center ;
+`
+const Card=styled.div`
+padding: 32px;
+width: 380px;
+height: 420px;
+/* White */
+background: #FFFFFF;
+/* Secondary / Black */
+border: 4px solid #000000;
+box-shadow: 4px 4px 0px #000000;
+border-radius: 12px;
+margin: 0 auto;
+`
 export default Design;
--- readyme-onair-front/src/routes/Posts.tsx
+++ readyme-onair-front/src/routes/Posts.tsx
@@ -3,57 +3,57 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Header from '../Components/Header';
 import styled from 'styled-components';
 import internal from 'stream';
+import Design from './Categories/Design';
+import All from './Categories/All';
+import Devlop from './Categories/Develop';
+import PD from './Categories/PD';
+import TeamBuild from './Categories/TeamBuild';
+import Daily from './Categories/Daily';
 
 // 게시글 리스트 페이지
 const Posts = () => {
-  const [isClick, setIsClick]=useState<boolean>(false);
-  const [posts, setPosts]=useState([
-    {
-      _id: "6226fed1aa058eaab230059e",
-      title: "테스트 글1",
-      content: "테스트 내용",
-      category: 1,
-      writer: "test1",
-      pw: 1234,
-      imageId: "이미지 번호",
-      date: "2022-03-08T06:59:29.920Z"
-  },
-  {
-    _id: "6226fed1aa058eaab230059e",
-    title: "테스트 글1",
-    content: "테스트 내용",
-    category: 1,
-    writer: "test1",
-    pw: 1234,
-    imageId: "이미지 번호",
-    date: "2022-03-08T06:59:29.920Z"
-},
-  ])
+  //검색->카테고리
+  const [designClick, setDesignClick]=useState<boolean>(false);
+  const [developClick, setDevelopClick]=useState<boolean>(false);
+  const [pdClick, setPdClick]=useState<boolean>(false);
+  const [teambuildClick, setTeambuildClick]=useState<boolean>(false);
+  const [dailyClick, setDailyClick]=useState<boolean>(false);
+
+  const [category, setCategory]=useState<string>("");
+  const categoryPost=()=>{
+    switch(category){
+      case "":
+        return (<All></All>);
+      case "design":
+        return(<Design></Design>);
+      case "develop":
+        return (<Devlop/>)
+      case "pd":
+        return (<PD/>)
+      case "teambuild":
+        return (<TeamBuild/>)
+      case "daily":
+        return (<Daily/>)
+    }
+  }
+  
+  
   useEffect(()=>{
    ;
   },[])
   return (
     <>
     <Container>
+     <input>
+     </input>
       <Category>
-        {/*<CategoryBtn isClick={isClick}>디자인</CategoryBtn>*/}
-        <CategoryBtn>개발</CategoryBtn>
-        <CategoryBtn>기획</CategoryBtn>
-        <CategoryBtn>프로젝트 모집</CategoryBtn>
-        <CategoryBtn>일상</CategoryBtn>
+        <CategoryBtn isClick={designClick} onClick={()=>category==="design"? (setCategory("") , setDesignClick(false)):(setCategory("design"),setDesignClick(true))}>디자인</CategoryBtn>
+        <CategoryBtn isClick={developClick} onClick={()=>category==="develop"? (setCategory(""), setDevelopClick(false)):(setCategory("develop"),setDevelopClick(true))}>개발</CategoryBtn>
+        <CategoryBtn isClick={pdClick} onClick={()=>category==="pd"? (setCategory(""), setPdClick(false)):(setCategory("pd"), setPdClick(true))}>기획</CategoryBtn>
+        <CategoryBtn isClick={teambuildClick} onClick={()=>category==="teambuild"? (setCategory(""), setTeambuildClick(false)):(setCategory("teambuild"),setTeambuildClick(true))}>프로젝트 모집</CategoryBtn>
+        <CategoryBtn isClick={dailyClick} onClick={()=>category==="daily"? (setCategory(""), setDailyClick(false)):(setCategory("daily"), setDailyClick(true))}>일상</CategoryBtn>
       </Category>
-      <PostsContainer>
-        {posts.map((item)=>{
-          return (
-            <Link to={`/post/${item._id}`} style={{textDecoration:"none"}}>
-              <Card>
-                <img src="../imgs/Image.png"></img>
-                <div>{item.title}</div>
-              </Card>  
-            </Link>
-        )})}
-        <Card></Card>
-      </PostsContainer>
+      {categoryPost()}
     </Container>
     </>
   ); 
@@ -67,36 +67,20 @@ const Category=styled.div`
   text-align: center;
   padding-top:30px ;
 `
-// background: ${props=>props.isClick ?'#366EFF' : '#fff'};
-
-const CategoryBtn=styled.button`
+type isClick={
+  isClick:boolean;
+};
+const CategoryBtn=styled.button<isClick>`
   height: 60px;
   font-size: 24px;
   padding: 12px 24px;
-  background:#fff;
+  background: #fff;
+  color: ${props=>props.isClick ?'#366EFF' : 'black'};
   border: 4px solid #000000;
   box-shadow: 4px 4px 0px #000000;
   border-radius: 12px;
   margin-left:50px ;
 `
-const PostsContainer=styled.div`
-  display:grid ;
-  grid-template-columns:repeat(auto-fill, minmax(33%,33%));
-  margin-top:50px ;
-  row-gap:30px;
-  grid-template-rows:1fr 1fr 1fr;
-  text-align:center ;
-`
-const Card=styled.div`
-padding: 32px;
-width: 380px;
-height: 420px;
-/* White */
-background: #FFFFFF;
-/* Secondary / Black */
-border: 4px solid #000000;
-box-shadow: 4px 4px 0px #000000;
-border-radius: 12px;
-margin: 0 auto;
-`
+
+
 export default Posts;
